610f6d982f74d08cf38224fb9eb86ce000bfc5ca
mawiaweb <tech.mawia@gmail.com>
2018-03-19 22:25:34 -0500

    Form Handler - Assignment

---
+++ formHandler.php
@@ -0,0 +1,71 @@
+<?php
+//Model-Controller Area.  The PHP processing code goes in this area. 
+
+	//Method 1.  This uses a loop to read each set of name-value pairs stored in the $_POST array
+	$tableBody = "";		//use a variable to store the body of the table being built by the script
+	
+	foreach($_POST as $key => $value)		//This will loop through each name-value in the $_POST array  (Note equal arrows sign means associate ka belh) post variable ah hian kan va thawn lut he hian a lo validate
+	{
+		$tableBody .= "<tr>";				//formats beginning of the row
+		$tableBody .= "<td>$key</td>";		//dsiplay the name of the name-value pair from the form
+		$tableBody .= "<td>$value</td>";	//dispaly the value of the name-value pair from the form
+		$tableBody .= "</tr>";				//End this row
+	} 
+	 //a chung .(dot) hi concatenate duh vang a nia, <tr> <td> he mi value lak duh vang in
+	
+	//Method 2.  This method pulls the individual name-value pairs from the $_POST using the name
+	//as the key in an associative array.  
+	
+	$inFirstName = $_POST["firstName"];		//Get the value entered in the first name field
+	$inLastName = $_POST["lastName"];		//Get the value entered in the last name field
+	$inSchool = $_POST["school"];			//Get the value entered in the school field
+	$hearAbout = $_POST["about"]; 		//Get the value entered of the selected radio button 
+	$book = "";						//Variable to hold value of the checkbox
+	$textBook = $_POST["bookName"]; // Value entered of the selected dropdown
+
+
+	if(!empty($_POST["ebook"]))   //get value entered of the selected checkbox
+	{
+		foreach ($_POST["ebook"] as $selected) {
+			$book = $book . " " . $selected;
+		}
+	}
+
+	else
+	{
+		$book = "None";
+	}
+
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>WDV 341 Intro PHP - Code Example</title>
+</head>
+
+<body>
+<h1>WDV341 Intro PHP</h1>
+<h2>Form Handler Result Page - Code Example</h2>
+<p>This page displays the results of the Server side processing. </p>
+<p>The PHP page has been formatted to use the Model-View-Controller (MVC) concepts. </p>
+<h3>Display the values from the form using Method 1. Uses a loop to process through the $_POST array</h3>
+<p>
+	<table border='a'>
+    <tr>
+    	<th>Field Name</th>
+        <th>Value of Field</th>
+    </tr>
+	<?php echo $tableBody;  ?>
+	</table>
+</p>
+<h3>Display the values from the form using Method 2. Displays the individual values.</h3>
+<p>School: <?php echo $inSchool; ?></p>
+<p>First Name: <?php echo $inFirstName; ?></p>
+<p>Last Name: <?php echo $inLastName; ?></p>
+<p>About: <?php echo $hearAbout; ?></p>
+<p>Printed Version and Ebook: <?php echo $book; ?></p>
+<p>Select your textbook: <?php echo $textBook; ?></p>
+
+
+</body>
+</html>
